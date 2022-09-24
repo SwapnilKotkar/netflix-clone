ee06b033794c4ea996ca57c0194e9968d11318f1
swapnil <swapnilkotkar793@gmail.com>
2022-09-24 20:48:37 +0530

    adjusted height of posters

--- components/Row.jsx
+++ components/Row.jsx
@@ -21,11 +21,11 @@ const Row = ({ title, fetchURL, isLargeRow, wait }) => {
   return (
     <>
       <div className="mb-7">
-        <h2 className="text-white text-2xl font-bold my-5">{title}</h2>
+        <h2 className="text-white text-xl md:text-2xl font-bold my-5">{title}</h2>
           <div className="w-full flex overflow-x-scroll overflow-y-hidden">
             {movies?.map((movie) => (
               <img
-                className={`${isLargeRow ? 'h-[300px]' : 'h-[170px]'} p-2 first:pl-0 object-contain hover:scale-110 ease-in-out duration-300 cursor-pointer`}
+                className={`${isLargeRow ? 'h-[200px] md:h-[300px]' : 'h-[120px] md:h-[170px]'} p-2 first:pl-0 object-contain hover:scale-110 ease-in-out duration-300 cursor-pointer`}
                 key={movie.id}
                 src={`${imageBaseURL}${isLargeRow ? movie?.poster_path : movie?.backdrop_path}`}
                 alt={movie?.name}
