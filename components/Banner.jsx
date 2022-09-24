597b8055684fe9a2fe3ca38fd4c65e90c7bb7813
swapnil <swapnilkotkar793@gmail.com>
2022-09-24 20:14:03 +0530

    made banner page mobile responsive

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -5,6 +5,10 @@ import requests from '../requests/requests'
 const Banner = () => {
     const [movie, setMovie] = useState([])
 
+    const truncate = (str, n) => {
+        return str?.length > n ? str.substr(0, n-1) + "..." : str;
+    }
+
     useEffect(() => {
         async function fetchData() {
           const data = await instance.get(requests.fetchNetflixOriginals);
@@ -23,18 +27,19 @@ const Banner = () => {
                 backgroundPosition: "center center"
             }
         }>
-            <div className='h-[35rem] flex items-center px-10 bg-black bg-opacity-40'>
+            <div className='h-[35rem] flex items-end px-5 md:px-10 bg-black bg-opacity-50'>
             <div>
-                <h1 className='text-6xl font-bold mb-3'>{movie?.title || movie?.name || movie?.original_name}</h1>
+                <h1 className='text-4xl md:text-6xl font-bold mb-3'>{movie?.title || movie?.name || movie?.original_name}</h1>
 
-                <div className='my-10'>
-                    <button className='bg-slate-900 bg-opacity-70 mx-2 px-8 py-2'>Play</button>
-                    <button className='bg-slate-900 bg-opacity-70 mx-2 px-8 py-2'>My List</button>
+                <div className='my-5 md:my-10 space-x-2'>
+                    <button className='bg-slate-900 bg-opacity-70 px-4 md:px-8 py-1 md:py-2'>Play</button>
+                    <button className='bg-slate-900 bg-opacity-70 px-4 md:px-8 py-1 md:py-2'>My List</button>
                 </div>
 
-                <h1 className='w-2/3 text-lg font-semibold'>{movie?.overview}</h1>
+                <h1 className='w-full md:w-2/3 mb-2 text-sm md:text-lg font-semibold'>{truncate(movie?.overview, 150)}</h1>
             </div>
             </div>
+
         </div>
     </>
   )
