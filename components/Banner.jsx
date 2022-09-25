9b4a80dac147b1ffae46fb932e4581bbf4497679
swapnil <swapnilkotkar793@gmail.com>
2022-09-25 14:55:46 +0530

    updated

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -6,13 +6,13 @@ const Banner = () => {
     const [movie, setMovie] = useState([])
 
     const truncate = (str, n) => {
-        return str?.length > n ? str.substr(0, n-1) + "..." : str;
+        return str?.length > n ? str.slice(0, n) + "..." : str;
     }
 
     useEffect(() => {
         async function fetchData() {
           const data = await instance.get(requests.fetchNetflixOriginals);
-          setMovie(data.data.results[Math.floor(Math.random() * data.data.results.length - 1)]);
+          setMovie(data.data.results[Math.floor(Math.random() * data.data.results.length)]);
           return data;
         }
         fetchData();
@@ -27,16 +27,16 @@ const Banner = () => {
                 backgroundPosition: "center center"
             }
         }>
-            <div className='h-[25rem] md:h-[35rem] flex items-end px-5 md:px-10 bg-black bg-opacity-50'>
-            <div>
-                <h1 className='text-4xl md:text-6xl font-bold mb-3'>{movie?.title || movie?.name || movie?.original_name}</h1>
+            <div className='h-[25rem] md:h-[35rem] flex items-end md:items-center px-5 md:px-10 bg-black bg-opacity-50'>
+            <div className='space-y-5'>
+                <h1 className='text-4xl md:text-5xl font-bold'>{movie?.title || movie?.name || movie?.original_name}</h1>
 
-                <div className='my-5 md:my-10 space-x-2'>
+                <div className='md:my-10 space-x-2'>
                     <button className='bg-slate-900 bg-opacity-70 px-6 md:px-8 py-1 md:py-2'>Play</button>
                     <button className='bg-slate-900 bg-opacity-70 px-6 md:px-8 py-1 md:py-2'>My List</button>
                 </div>
 
-                <h1 className='w-full md:w-2/3 mb-2 text-sm md:text-lg font-semibold'>{truncate(movie?.overview, 150)}</h1>
+                <h1 className='w-full md:w-2/3 text-sm md:text-lg font-semibold'>{truncate(movie?.overview, 200)}</h1>
             </div>
             </div>
 
