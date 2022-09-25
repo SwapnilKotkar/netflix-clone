41446aa88f65561ad421f8ee9d7f3d39e1ea58d6
swapnil <swapnilkotkar793@gmail.com>
2022-09-25 14:55:21 +0530

    added row ID

--- components/MoviesCategory.jsx
+++ components/MoviesCategory.jsx
@@ -5,14 +5,14 @@ import requests from '../requests/requests'
 const MoviesCategory = () => {
   return (
     <>
-        <Row title="NETFLIX ORIGINALS" fetchURL={requests.fetchNetflixOriginals} isLargeRow/>
-        <Row title="Trending Now" fetchURL={requests.fetchTrending} wait={1000}/>
-        <Row title="Top Rated" fetchURL={requests.fetchTopRated} wait={1300}/>
-        <Row title="Action Movies" fetchURL={requests.fetchActionMovies} wait={1600}/>
-        <Row title="Comedy Movies" fetchURL={requests.fetchComedyMovies} wait={1900}/>
-        <Row title="Horror Movies" fetchURL={requests.fetchAHorrorMovies} wait={2200}/>
-        <Row title="Romance Movies" fetchURL={requests.fetchARomanceMovies} wait={2500}/>
-        <Row title="Documentries" fetchURL={requests.fetchDocumentries} wait={2800}/>
+        <Row RowID='1' title="NETFLIX ORIGINALS" fetchURL={requests.fetchNetflixOriginals} isLargeRow/>
+        <Row RowID='2' title="Trending Now" fetchURL={requests.fetchTrending} wait={1000}/>
+        <Row RowID='3' title="Top Rated" fetchURL={requests.fetchTopRated} wait={1300}/>
+        <Row RowID='4' title="Action Movies" fetchURL={requests.fetchActionMovies} wait={1600}/>
+        <Row RowID='5' title="Comedy Movies" fetchURL={requests.fetchComedyMovies} wait={1900}/>
+        <Row RowID='6' title="Horror Movies" fetchURL={requests.fetchAHorrorMovies} wait={2200}/>
+        <Row RowID='7' title="Romance Movies" fetchURL={requests.fetchARomanceMovies} wait={2500}/>
+        <Row RowID='8' title="Documentries" fetchURL={requests.fetchDocumentries} wait={2800}/>
 
     </>
   )
